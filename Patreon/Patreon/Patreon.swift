730dcb6a769711d446c846c00fe6bda965fbb400
Foti Dim <>
2018-03-24 10:48:24 +0100

    Extracted private functions within resource()

--- Patreon/Patreon/Patreon.swift
+++ Patreon/Patreon/Patreon.swift
@@ -34,39 +34,49 @@ public class Patreon {
             completion?(result)
         }
     }
-
+    
     private func resource<T: Codable>(_ resource: String, queries: [URLQueryItem] = [], modelType: T.Type, accessToken: String? = nil, completion: ((Result<T>) -> Void)?) {
-        urlComponents.path = "/api/oauth2/api/" + resource
-        urlComponents.queryItems = queries
-        var request = URLRequest(url: urlComponents.url!)
+        func requestFor(resource: String) -> URLRequest {
+            urlComponents.path = "/api/oauth2/api/" + resource
+            urlComponents.queryItems = queries
+            return URLRequest(url: urlComponents.url!)
+        }
         
-        if accessToken == nil {
-            if let data = KeyChain.load(key: "AccessToken"),
-                let result = String(data: data, encoding: String.Encoding.utf8){
-                request.setValue("Bearer " + result, forHTTPHeaderField: "Authorization")
+        func urlRequest(_ request: URLRequest, withAccessToken accessToken: String?) -> URLRequest {
+            var authenticatedRequest = request
+            if let accessToken = accessToken {
+                authenticatedRequest.setValue(accessToken, forHTTPHeaderField: "Authorization")
             }
-        }
-        else {
-            request.setValue(accessToken, forHTTPHeaderField: "Authorization")
-            
+            else {
+                if let accessTokenData = KeyChain.load(key: "AccessToken"),
+                    let accessToken = String(data: accessTokenData, encoding: String.Encoding.utf8){
+                    authenticatedRequest.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+                }
+            }
+            return authenticatedRequest
         }
         
-        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-            DispatchQueue.main.async {
-                if let error = error {
-                    completion?(.failure(error))
-                }
-                else if let httpResponse = response as? HTTPURLResponse {
-                    let statusCode = httpResponse.statusCode
-                    
-                    if (statusCode != 200) {
-                        let error = HTTPStatusCode(rawValue: statusCode)!
+        func fetchData(of modelType: T.Type, using request: URLRequest, _ completion: ((Result<T>) -> Void)?) {
+            let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+                DispatchQueue.main.async {
+                    if let error = error {
                         completion?(.failure(error))
                         return
                     }
-                }
-                
-                if let jsonData = data {
+                    
+                    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                        if let httpError = HTTPStatusCode(rawValue: httpResponse.statusCode) {
+                            completion?(.failure(httpError))
+                        }
+                        return
+                    }
+                    
+                    guard let jsonData = data else {
+                        let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data was not retrieved from request"]) as Error
+                        completion?(.failure(error))
+                        return
+                    }
+                    
                     let decoder = JSONDecoder()
                     decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601)
                     
@@ -76,13 +86,14 @@ public class Patreon {
                     } catch {
                         completion?(.failure(error))
                     }
-                } else {
-                    let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data was not retrieved from request"]) as Error
-                    completion?(.failure(error))
                 }
             }
+            
+            task.resume()
         }
         
-        task.resume()
+        var request = requestFor(resource: resource)
+        request = urlRequest(request, withAccessToken: accessToken)
+        fetchData(of: modelType, using: request, completion)
     }
 }
